c24371c20ab843775fced60f35f60f64ab29369f
Francisco Dadone <49202617+FranciscoDadone@users.noreply.github.com>
2021-04-14 19:57:45 -0300

    Add files via upload

--- tp2/main.cpp
+++ tp2/main.cpp
@@ -1,41 +1,30 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
 #include "edya2.h"
-
-
 using namespace std;
-
 int main()
 {
-    //cout << palindrome("reconocer") << endl;
-
-    /*
-    vector<unsigned int> v = create(30, 3, 20);
-    for(unsigned int i: v) {
-        cout << i << endl;
-    }
-    */
-
-    //vector<unsigned int> v = {11, 40, 41, 13, 55, 90, 1, 3};
-    //print(v, FMT_LINE);
-    //sort(v); print(v);
-
-    /*
-    vector<unsigned int> v1 = {1, 2, 3, 4};
-    vector<unsigned int> v2 = {6, 7, 8};
-
-    vector<unsigned int> v3 = insert(v1, v2, 1);
-    print(v3);
-    */
+    //################# Ejercicio 1 ###################
+    string str="reconocer";
+    /*Metodo_at(str);
+    cout<<endl;
+    operador_de_inter(str);
+    cout<<endl;
+    iterador(str);
+    cout<<endl;
+    bucle_rang(str);
+    cout<<endl;*/
+    //################# Ejercicio 2 ###################
+    string ret;
+    //ret=palindromePyramid(str);
+    //cout<<ret<<endl;
 
 
+    //########### Ejercicio 3 #############
 
-    /*
-    vector<unsigned int> v1 = {1, 2, 3, 4, 1, 1, 8, 0, 0, 0, 3};
-    print_frequency(v1);
-    */
-
-
+    //ret=replaceAll("supercalifragilisticoespialidoso","li","da");
+    //cout<<ret;
     return 0;
 }
+
+
+
