bfde6b52ac2da3fce47ded0071552c0ca30fd5ac
Francisco Dadone <49202617+FranciscoDadone@users.noreply.github.com>
2021-04-14 20:05:02 -0300

    reverted a commit

--- tp2/main.cpp
+++ tp2/main.cpp
@@ -1,30 +1,40 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 #include "edya2.h"
+
+
 using namespace std;
+
 int main()
 {
-    //################# Ejercicio 1 ###################
-    string str="reconocer";
-    /*Metodo_at(str);
-    cout<<endl;
-    operador_de_inter(str);
-    cout<<endl;
-    iterador(str);
-    cout<<endl;
-    bucle_rang(str);
-    cout<<endl;*/
-    //################# Ejercicio 2 ###################
-    string ret;
-    //ret=palindromePyramid(str);
-    //cout<<ret<<endl;
+    //cout << palindrome("reconocer") << endl;
+
+    /*
+    vector<unsigned int> v = create(30, 3, 20);
+    for(unsigned int i: v) {
+        cout << i << endl;
+    }
+    */
+
+    //vector<unsigned int> v = {11, 40, 41, 13, 55, 90, 1, 3};
+    //print(v, FMT_LINE);
+    //sort(v); print(v);
+
+    /*
+    vector<unsigned int> v1 = {1, 2, 3, 4};
+    vector<unsigned int> v2 = {6, 7, 8};
+    vector<unsigned int> v3 = insert(v1, v2, 1);
+    print(v3);
+    */
 
 
-    //########### Ejercicio 3 #############
 
-    //ret=replaceAll("supercalifragilisticoespialidoso","li","da");
-    //cout<<ret;
+    /*
+    vector<unsigned int> v1 = {1, 2, 3, 4, 1, 1, 8, 0, 0, 0, 3};
+    print_frequency(v1);
+    */
+
+
     return 0;
 }
-
-
-
