abe55c8919e8d7e92f34416a97b73dc49894ba8b
FranciscoDadone <dadonefrancisco@hotmail.com.ar>
2021-04-13 23:02:25 -0300

    Ejercicio 4 terminado. Y primeras funciones del 5 hechas

--- tp2/edya2.h
+++ tp2/edya2.h
@@ -2,8 +2,11 @@
 #define EDYA2_H_INCLUDED
 
 #include <stack>
+#include <vector>
 
 using namespace std;
+
+// ## EJERCICIO 4 ## //
 
 bool palindrome(const string& str) {
     stack <char> s;
@@ -17,8 +20,60 @@ bool palindrome(const string& str) {
         i++;
     }
     return true;
+    // Agregue el correspondiente control al EJERCICIO 2 para
+    // solo construir la pirámide en el caso de que la entrada sea palíndroma.
 }
 
 
+// ## EJERCICIO 5 ## //
+
+vector<unsigned int> create(unsigned int N, unsigned int from, unsigned int to) {
+    vector<unsigned int> v;
+    while(N != 0) {
+        v.push_back(rand() % (to - from + 1) + from);
+        N--;
+    }
+    return v;
+}
+
+enum format { FMT_LIST, FMT_LINE };
+void print(vector<unsigned int> v, format f=FMT_LIST) {
+    for(unsigned int i = 0; i < (unsigned int)v.size(); i++) {
+        cout << v.at(i) << ((f == FMT_LINE && i != (v.size() - 1)) ? ", " : "\n");
+    }
+}
+
+void sort(vector<unsigned int> &v) {
+    for(unsigned int i = 0; i <= v.size(); i++) {
+        bool cont = false;
+        for(unsigned int j = 0; j < (v.size() - 1); j++) {
+            if(v[j] > v[j + 1]) {
+                unsigned int temp = v[j];
+                v[j]              = v[j + 1];
+                v[j + 1]          = temp;
+                cont = true;
+            }
+        }
+        if(!cont) break;
+    }
+}
+
+vector<unsigned int> insert(vector<unsigned int> v1, vector<unsigned int> v2, unsigned int pos) {
+    vector<unsigned int> merged;
+
+    bool t = false;
+    for(int i = 0; i < ((v1.size() + v2.size()) - 1); i++) {
+        if(i == pos) t = true;
+
+        if(t) {
+            merged.push_back(v2.at(i-pos));
+        }
+
+        if(v2.size() > (i-pos)) t = false;
+    }
+
+    return merged;
+
+}
 
 #endif // EDYA2_H_INCLUDED
--- tp2/main.cpp
+++ tp2/main.cpp
@@ -7,6 +7,24 @@ using namespace std;
 
 int main()
 {
-    cout << palindrome("reconocer") << endl;
+    //cout << palindrome("reconocer") << endl;
+
+    /*
+    vector<unsigned int> v = create(10, 3, 20);
+    for(unsigned int i: v) {
+        cout << i << endl;
+    }
+    */
+
+    vector<unsigned int> v = {11, 40, 41, 13, 55, 90, 1, 3};
+    //print(v, FMT_LINE);
+    //sort(v); print(v);
+
+    vector<unsigned int> v1 = {1, 2, 3, 4};
+    vector<unsigned int> v2 = {6, 7, 8};
+
+    vector<unsigned int> v3 = insert(v1,v2,1);
+    print(v3);
+
     return 0;
 }
