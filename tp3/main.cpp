113042748eaa4ac5eaa30c1f08e84cf575ae2d16
FranciscoDadone <dadonefrancisco@hotmail.com.ar>
2021-04-20 09:16:38 -0300

    ejercicio 1 casi terminado. Falta el inciso 3

--- tp3/main.cpp
+++ tp3/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include "point.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    // # Ejercicio 1 # //
+    Point a;
+    cout << a.getX() << " " << a.getY() << endl;
+    a.setX(10);
+    a.setY(20);
+    cout << a.getX() << " " << a.getY() << endl;
+
+    Point b(40,50);
+    cout << b.getX() << " " << b.getY() << endl;
+
     return 0;
 }
---
+++ tp3/point.h
@@ -0,0 +1,45 @@
+#ifndef TP3_POINT_H
+#define TP3_POINT_H
+
+class Point {
+
+public:
+    // Constructors
+    Point() {
+        this -> x = 0;
+        this -> y = 0;
+    }
+
+    Point(int x, int y) {
+        this -> x = x;
+        this -> y = y;
+    }
+
+    // Methods implementation
+    int  getX();
+    int  getY();
+    void setX(int x);
+    void setY(int y);
+
+
+
+private:
+    int x, y;
+};
+
+
+
+// ## Methods ## //
+// Getters
+int Point::getX() { return this -> x; }
+int Point::getY() { return this -> y; }
+
+// Setters
+void Point::setX(int x) { this -> x = x; }
+void Point::setY(int y) { this -> y = y; }
+
+
+
+
+
+#endif //TP3_POINT_H
