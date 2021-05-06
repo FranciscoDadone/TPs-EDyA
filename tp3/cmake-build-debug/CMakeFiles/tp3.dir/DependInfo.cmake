5c424b6a1b68137c207efe2ae3a7bb5d3619ff81
FranciscoDadone <dadonefrancisco@hotmail.com.ar>
2021-05-06 17:39:38 -0300

    .

--- tp3/cmake-build-debug/CMakeFiles/tp3.dir/DependInfo.cmake
+++ tp3/cmake-build-debug/CMakeFiles/tp3.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/franciscodadone/Desktop/Facultad/EDyA/tp3/main.cpp" "/home/franciscodadone/Desktop/Facultad/EDyA/tp3/cmake-build-debug/CMakeFiles/tp3.dir/main.cpp.o"
+  "/home/franciscodadone/Desktop/Facultad Programación/EDyA/tp3/main.cpp" "/home/franciscodadone/Desktop/Facultad Programación/EDyA/tp3/cmake-build-debug/CMakeFiles/tp3.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
