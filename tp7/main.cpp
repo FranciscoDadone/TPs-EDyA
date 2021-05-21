28b5a83d5e610813b5c9425580d275b8a0613d87
FranciscoDadone <dadonefrancisco@hotmail.com.ar>
2021-05-21 09:11:46 -0300

    merged

--- tp7/main.cpp
+++ tp7/main.cpp
@@ -1,43 +1,59 @@
 #include <iostream>
+#include <list>
+#include "FilaA.h"
 #include <stack>
 #include "pila_a.h"
 #include "pila_p.h"
-using namespace std;
-string preToInfix(string pre_exp);
-int main()
-{
-    /*PilaA<int> num;
-    num.apilar(1);
-    num.apilar(2);
-    num.apilar(3);
-    num.apilar(4);
-    num.apilar(5);
-    swap(num);
-    while(!num.estaVacia()){
-        cout<<num.recuperarTope()<<endl;
-        num.desapilar();
-    }*/
-    /*string x="* - A B / + C D * X Y";
-    x=preToInfix(x);
-    cout<<x;*/
-    Stack<int> x;
-    x.push(1);
-    x.push(2);
-    x.push(3);
-    x.push(4);
-    x.push(5);
 
-    if(x.empty()){
-        cout<<"esta vacia ["<<x.size()<<"]";
+using namespace std;
+
+/**
+ * Prints stack in reverse order recursively.
+ * @tparam T
+ * @param s
+ */
+template <class T> void print(stack<T> s) {
+    if (!s.empty()) {
+        T element = s.top();
+        s.pop();
+        print(s);
+        cout << element << " ";
+        s.push(element);
+    }
+}
+
+list<string> generarNBinarios(unsigned int n){
+    FilaA <string> f;
+    list<string> l;
+    if(n==0){
+        f.vaciar();
+        for (unsigned int i=0; i<n ;i++){
+            l.push_back(f.recuperarFrente());
+            f.quitar();
+        }
+        return l;
     }
     else{
-        cout<<"no esta vacia ["<<x.size()<<"]";
+        f.poner("1");
+        for(unsigned int i = 1; i< n; i++){
+            string a = f.recuperarFrente();
+            f.quitar();
+            string b = a;
+            f.poner(a.append("0"));
+            f.poner(b.append("1"));
+        }
+        for(unsigned int i=0; i< n ;i++){
+            l.push_back(f.recuperarFrente());
+            f.quitar();
+        }
+        return l;
     }
-    while(!x.empty()){
-        cout<<x.top()<<endl;
-        x.pop();
+}
+void print(list<string> const &list)
+{
+    for (auto const &i: list) {
+        cout << i << endl;
     }
-    return 0;
 }
 
 string preToInfix(string pre_exp){
@@ -74,4 +90,88 @@ string preToInfix(string pre_exp){
         }
     }
     return ret;
+}
+
+int main() {
+//    stack<int> s;
+//
+//    s.push(1);
+//    s.push(2);
+//    s.push(3);
+//    s.push(4);
+//    s.push(5);
+//
+//    print(s);
+//
+//    cout << "\n";
+//
+//    FilaA<int> f, g;
+//    f.poner(1);
+//    f.poner(2);
+//    f.poner(3);
+//    f.poner(4);
+//    f.poner(5);
+//    cout << f << endl;
+//
+//    g.poner(1);
+//    g.poner(2);
+//    g.poner(3);
+//    g.poner(4);
+//    g.poner(5);
+//
+//    cout << (f == g) << endl;
+//    cout << (f != g) << endl;
+
+
+    //cout<<f.recuperarUltimo()<<endl;
+    //f.quitarN(2);
+    //f.ponerAlFrente(15);
+    //f.ponerAlFrente(15);
+    // f.ponerAlFrente(15);
+    //f.ponerAlFrente(15);
+    //f.invertir();
+//    print(generarNBinarios(2));
+    /*
+    const int k= f.cantidad();
+    for(int i=0; i<k ;i++){
+       cout<< f.recuperarFrente()<<endl;
+       f.quitar();
+    }
+    */
+
+
+    /*PilaA<int> num;
+    num.apilar(1);
+    num.apilar(2);
+    num.apilar(3);
+    num.apilar(4);
+    num.apilar(5);
+    swap(num);
+    while(!num.estaVacia()){
+        cout<<num.recuperarTope()<<endl;
+        num.desapilar();
+    }*/
+    /*string x="* - A B / + C D * X Y";
+    x=preToInfix(x);
+    cout<<x;*/
+//    Stack<int> x;
+//    x.push(1);
+//    x.push(2);
+//    x.push(3);
+//    x.push(4);
+//    x.push(5);
+//
+//    if(x.empty()){
+//        cout<<"esta vacia ["<<x.size()<<"]";
+//    }
+//    else{
+//        cout<<"no esta vacia ["<<x.size()<<"]";
+//    }
+//    while(!x.empty()){
+//        cout<<x.top()<<endl;
+//        x.pop();
+//    }
+
+
+    return 0;
 }
